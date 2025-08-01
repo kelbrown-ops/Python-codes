158d3696ff30af7a99b990562de7958efd2c8422
kelbrown-ops <kelvinakwasic@gmail.com>
2025-08-01 03:13:02 +0000

    Add files via upload

---
+++ 99_bottles of beer song.py
@@ -0,0 +1,10 @@
+#string interpolation
+
+print("99 BOTTLES OF BEER 🍻🍺")
+print("    ")
+
+for i in range(99, 0, -1):
+  print(str(i)+" bottles of beer 🍻🍺 on the wall")
+  print(str(i)+" bottles of beer")
+  print("Take one down, pass it around")#
+  print("    ")
